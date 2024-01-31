382f4411f05bf725d87a758bea33d81d56d25eb2
tapatap1 <137928918+tapatap1@users.noreply.github.com>
2024-02-01 00:58:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,7 @@ const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
-
-
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
-	user.gamesPlayed(games);
-});
 
 
 // var username2 = process.env.username2;
